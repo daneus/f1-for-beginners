756a31c9645a3a30023e847dcef3782c02a3e757
Ezkrman <tony.almonacid@hotmail.com>
2023-02-21 09:14:23 -0500

    Added title, posterior implementation for desktop design pending

--- components/FirstSection/Header.tsx
+++ components/FirstSection/Header.tsx
@@ -5,14 +5,22 @@ import F1 from '../../public/logos/Formula1.svg'
 const Header: React.FC = () => {
   return (
     <>
-      <div className="-top-8 left-0 w-screen">
-        <Image className="w-screen" src={BG1} alt={'Formula 1 race'}></Image>
-        <div className="absolute h-full w-full top-0 left-0 shadow-[inset_0_-25vmin_25vmax_black] bg-dim"></div>
+      <div className="relative">
+        <div className="-top-8 left-0 w-screen">
+          <Image className="w-screen" src={BG1} alt={'Formula 1 race'}></Image>
+          <div className="absolute h-full w-full top-0 left-0 shadow-[inset_0_-25vmin_20vmax_black] bg-dim"></div>
+        </div>
+        <div className="absolute w-full flex justify-center items-center top-[10vmin]">
+          <Image
+            className="w-[57.5vmin]"
+            src={F1}
+            alt={'Formula 1 Logo'}
+          ></Image>
+        </div>
+        <div className="text-white xl:text-8xl text-3xl font-f1regular text-center w-screen top-[25vmin] absolute">
+          for beginners
+        </div>
       </div>
-      <div className="absolute w-full flex justify-center items-center top-[10vmin]">
-        <Image className="w-[57.5vmin]" src={F1} alt={'Formula 1 Logo'}></Image>
-      </div>
-      <div className="text-white font-f1regular">Pinga</div>
     </>
   )
 }
