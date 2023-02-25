ec934674debe9101d54b1e77ed967c9cb1a86743
Ezkrman <tony.almonacid@hotmail.com>
2023-02-25 01:18:58 -0500

    Improved font sizes

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -18,18 +18,18 @@ const Header: React.FC = () => {
         ></Image>
         <div className="absolute w-full flex flex-col justify-center items-center my-auto">
           <Image
-            className="w-[30vmax]"
+            className="w-[32vmax]"
             src={F1Logo}
             alt={'Formula 1 Logo'}
           ></Image>
-          <div className="text-[4.6vmax] text-white font-f1regular whitespace-nowrap">
+          <div className="text-[4.8vmax] text-white font-f1regular whitespace-nowrap">
             for beginners
           </div>
-          <div className="mt-[8vmax] flex flex-col gap-[.5vmax] self-start ml-[7vmin]">
-            <div className="text-white text-[2.9vmax] font-f1bold">
+          <div className="w-full text-center mt-[6.5vmax] flex flex-col gap-[.5vmax] self-start">
+            <div className="text-white text-[3.1vmax] font-f1bold">
               What is Formula 1?
             </div>
-            <div className="text-[2.2vmax] font-f1regular text-[#868F97] leading-tight">
+            <div className="text-[2.4vmax] font-f1regular text-[#868F97] leading-tight">
               Formula 1 is the
               <br />
               <span className="whitespace-nowrap animate-[gradient_5s_linear_infinite] font-f1bold bg-[length:200%] bg-gradient-to-r from-[#7FC2FE] via-[#FF5656] to-[#7FC2FE] bg-clip-text text-transparent">
