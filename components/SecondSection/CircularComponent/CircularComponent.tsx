17835ace9dea80c8b43cacbe4f7ed44cd8baa328
Ezkrman <tony.almonacid@hotmail.com>
2023-02-25 13:03:08 -0500

    Added layout for decagonal model

--- components/SecondSection/CircularComponent/CircularComponent.tsx
+++ components/SecondSection/CircularComponent/CircularComponent.tsx
@@ -1,6 +1,17 @@
 const CircularComponent: React.FC = () => {
   return (
-    <div className="mt-[2.4vw] w-[100vmin] h-[100vmin] mx-auto border-2 border-white"></div>
+    <div className="my-[7.6vmax] w-[78.9vmin] h-[75vmin] mx-auto border-2 border-white relative">
+      <div className="w-[20%] bg-white h-[20%] absolute top-[0] left-[34.55%] translate-x-[-50%] translate-y-[-50%]"></div>
+      <div className="w-[20%] bg-white h-[20%] absolute top-[0] right-[34.55%] translate-x-[50%] translate-y-[-50%]"></div>
+      <div className="w-[20%] bg-white h-[20%] absolute top-[19%] left-[10%] translate-x-[-50%] translate-y-[-50%]"></div>
+      <div className="w-[20%] bg-white h-[20%] absolute top-[19%] right-[10%] translate-x-[50%] translate-y-[-50%]"></div>
+      <div className="w-[20%] bg-white h-[20%] absolute top-[50%] left-[0%] translate-x-[-50%] translate-y-[-50%]"></div>
+      <div className="w-[20%] bg-white h-[20%] absolute top-[50%] right-[0%] translate-x-[50%] translate-y-[-50%]"></div>
+      <div className="w-[20%] bg-white h-[20%] absolute bottom-[19%] left-[10%] translate-x-[-50%] translate-y-[50%]"></div>
+      <div className="w-[20%] bg-white h-[20%] absolute bottom-[19%] right-[10%] translate-x-[50%] translate-y-[50%]"></div>
+      <div className="w-[20%] bg-white h-[20%] absolute bottom-[0%] left-[34.55%] translate-x-[-50%] translate-y-[50%]"></div>
+      <div className="w-[20%] bg-white h-[20%] absolute bottom-[0%] right-[34.55%] translate-x-[50%] translate-y-[50%]"></div>
+    </div>
   )
 }
 
